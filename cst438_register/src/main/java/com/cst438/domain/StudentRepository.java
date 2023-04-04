39a8ffcc11a63af17e96be9707fbdef773324b03
Nicole <nigeorge@csumb.edu>
2023-04-03 19:53:14 -0700

    assignemnt5

--- cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
@@ -11,15 +11,15 @@ public interface StudentRepository extends CrudRepository <Student, Integer> {
 	// declare the following method to return a single Student object
 	// default JPA behavior that findBy methods return List<Student> except for findById.
 	public Student findByEmail(String email);
-//   @Query("select e from Student e where e.student.email=:email and e.student_id=:id")
-//   public List<Student> findStudent(
-//         @Param("email") String email, 
-//         @Param("id") int student_id);
-//   public int findById(
-//         @Param("id") int student_id);
+   @Query("select e from Student e where e.student.email=:email and e.student_id=:id")
+   public List<Student> findStudent(
+         @Param("email") String email, 
+         @Param("id") int student_id);
+   public int findById(
+         @Param("id") int student_id);
 //   
-//   @Query("select e from Student e where e.student.email=:email and e.student.student_id=:student_id")
-//   Student findByEmailAndStudentId(@Param("email") String email, @Param("student_id") int student_id);
+   @Query("select e from Student e where e.student.email=:email and e.student.student_id=:student_id")
+   Student findByEmailAndStudentId(@Param("email") String email, @Param("student_id") int student_id);
    
    @SuppressWarnings("unchecked")
    Student save(Student e);
